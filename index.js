1b910e7a258ffdaed728d34017d7e947239ba972
Diego Celis <diegofercelis@gmail.com>
2021-04-08 17:19:36 -0300

    metodos post, put e delete funcionando corretamente, testados com o software insomnia

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //Módulos:
 const colors = require ('colors')
 const express = require ('express')
-const { Sequelize, DataTypes } = require ('sequelize')
+const { Sequelize, DataTypes } = require ('sequelize');
+const task = require('./models/task');
 
 //Especificação da BD:
 const app = express ();
@@ -15,27 +16,68 @@ const Task = require ('./models/task')
 const tasks = Task ( sequelize, DataTypes)
 
 // We need to parse JSON coming from requests
-// app.use(express.json()) //PARA QUE ISSO??
+app.use(express.json()) //PARA QUE ISSO??
 
 
 app.get('', (req, res) =>{
     res.send ('<h1>Página principal</h1>')
 })
 
-//Mostrar todas as tarefas
+//GET Mostrar todas as tarefas
 app.get('/tasks', async (req, res) =>{
     
     const tarefas = await tasks.findAll()
-    res.send (tarefas) //funciona igual
+    // res.send (tarefas) //funciona igual
     // res.json ({ action: tarefas })
+    res.json ({ tarefas })
+
 })
 
-//POR ID:
+//GET Mostrar UMA determinada tarefa por ID:
 app.get('/tasks/:id', async (req, res) =>{
     
     const tarefa_ID = req.params.id //Aqui tô pegando o parametro id da requisição app.get('/tasks/:id'... [os dois pontos chutam esse parametro para o "params"]
     const tarefa = await tasks.findByPk(tarefa_ID)
-    res.json({ action: tarefa })
+    res.json({ tarefa })
+})
+
+//POST Criar tarefas
+app.post('/tasks', async (req, res) =>{
+    const body = req.body
+    
+    const nova_tarefa = await tasks.create({
+        description: body.description,
+        done: body.done
+    })
+    
+    res.json({ nova_tarefa })
+    
+    // const nova_tarefa = await tasks.create({
+    //     description: b.description,
+    //     done: true
+    // })
+    // res.send({ nova_tarefa })
+})
+
+//PUT Atualizar uma determinada tarefa
+app.put('/tasks/:id', async (req, res) =>{
+    const tarefa_ID = req.params.id
+    const body = req.body
+    const tarefa = await tasks.findByPk(tarefa_ID)
+
+    tarefa.update({
+        description: body.description,
+        done: body.done
+      });
+
+    res.send({ action: 'Atualizando tarefa', tarefa: tarefa })
+})
+
+//DELETE Apagar uma determinada tarefa
+app.delete('/tasks/:id', async (req, res) =>{
+    const ID = req.params.id
+    const apagando_tarefa = await tasks.destroy({ where: { ID: ID } })
+    res.send({ apagando_tarefa })
 })
 
 

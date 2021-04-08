d4129104631840780aa64726c70f9d929d5efbe5
Diego Celis <diegofercelis@gmail.com>
2021-04-08 11:31:01 -0300

    implementacao do sequelize e sequelize-cli,.aAém de declarar o tipo de BD usada(sqlite) no meu index.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 //Módulos:
 const colors = require ('colors')
 const express = require ('express')
+const { Sequelize, DataTypes } = require ('sequelize')
 
-const app = express ()
+
+const app = express ();
+const sequelize = new Sequelize ({
+    dialect: 'sqlite',
+    storage:'./task-list.db'
+});
+
+
 
 
 app.get('', (req, res) =>{
     res.send ('<h1>Página Principal!</h1>')
+})
+
+app.get('/task', (req, res) =>{
+
 })
 
 

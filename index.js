5f2677eb0197a8c64ba5572bad9389e1832775c0
Diego Celis <diegofercelis@gmail.com>
2021-04-08 11:16:56 -0300

    instaladas as dependencias, criacao do gitignore e modificacao do package.json para executar mais facilmente o projeto com ajuda do nodemon tambem

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const colors = require ('colors')
 const express = require ('express')
 
 const app = express ()
+
+
+app.get('', (req, res) =>{
+    res.send ('<h1>Página Principal!</h1>')
+})
+
+
+app.listen (5200, () => {
+    console.log('Server funcionando no porto 5200'. rainbow)
+})

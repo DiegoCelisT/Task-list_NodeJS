83ed55ede96cc50fa94a57a6d8f7619982d299c9
Diego Celis <diegofercelis@gmail.com>
2021-04-09 00:33:00 -0300

    Implementacao de try/catch para pegar os erros e mostrar respostas mais amigÃaveis

--- index.js
+++ index.js
@@ -15,73 +15,66 @@ const sequelize = new Sequelize ({
 const Task = require ('./models/task')
 const tasks = Task ( sequelize, DataTypes)
 
-// We need to parse JSON coming from requests
-app.use(express.json()) //PARA QUE ISSO??
+// Para analisar os JSON provenientes das requisições (Sem isso não tem como interpretar o que chega)
+app.use(express.json()) 
 
-
+//Página Principal
 app.get('', (req, res) =>{
     res.send ('<h1>Página principal</h1>')
 })
 
 //GET Mostrar todas as tarefas
-app.get('/tasks', async (req, res) =>{
-    
+app.get('/tasks', async (req, res) =>{    
     const tarefas = await tasks.findAll()
+    res.json ({ tarefas })
     // res.send (tarefas) //funciona igual
     // res.json ({ action: tarefas })
-    res.json ({ tarefas })
-
 })
 
 //GET Mostrar UMA determinada tarefa por ID:
 app.get('/tasks/:id', async (req, res) =>{
-    
     const tarefa_ID = req.params.id //Aqui tô pegando o parametro id da requisição app.get('/tasks/:id'... [os dois pontos chutam esse parametro para o "params"]
     const tarefa = await tasks.findByPk(tarefa_ID)
     res.json({ tarefa })
 })
 
-//POST Criar tarefas
+//POST Criar tarefas:
 app.post('/tasks', async (req, res) =>{
     const body = req.body
-    
     const nova_tarefa = await tasks.create({
         description: body.description,
         done: body.done
     })
-    
     res.json({ nova_tarefa })
-    
-    // const nova_tarefa = await tasks.create({
-    //     description: b.description,
-    //     done: true
-    // })
-    // res.send({ nova_tarefa })
 })
 
-//PUT Atualizar uma determinada tarefa
+//PUT Atualizar uma determinada tarefa (uso de try catch para pegar os erros e que não fique carregando):
 app.put('/tasks/:id', async (req, res) =>{
-    const tarefa_ID = req.params.id
-    const body = req.body
-    const tarefa = await tasks.findByPk(tarefa_ID)
-
-    tarefa.update({
-        description: body.description,
-        done: body.done
-      });
-
-    res.send({ action: 'Atualizando tarefa', tarefa: tarefa })
+    try{
+        const tarefa_ID = req.params.id
+        const body = req.body
+        const tarefa = await tasks.findByPk(tarefa_ID)
+        tarefa.update({
+            description: body.description,
+            done: body.done
+        });        
+        res.send({ action: 'Atualizando tarefa', tarefa: tarefa })
+    } catch (error) {
+        return res.send( `<h1>Esta é uma mensagem amigável de erro :P</h1><br><h2>O que aconteceu foi o seguinte:</h2><br><h2>${error}</h2>`)
+    }
 })
 
 //DELETE Apagar uma determinada tarefa
-app.delete('/tasks/:id', async (req, res) =>{
-    const ID = req.params.id
-    const apagando_tarefa = await tasks.destroy({ where: { ID: ID } })
-    res.send({ apagando_tarefa })
+app.delete('/tasks/:id', async (req, res) => {
+    try{
+        const ID = req.params.id
+        const apagando_tarefa = await tasks.destroy({ where: { ID: ID } })
+        res.send({ action: 'Apagando tarefa', apagando_tarefa: apagando_tarefa })
+    } catch (error) {
+        return res.send( `<h1>Esta é uma mensagem amigável de erro :P</h1><br><h2>O que aconteceu foi o seguinte:</h2><br><h2>${error}</h2>`)
+    }
 })
 
-
-
-app.listen (5200, () => {
-    console.log('Server funcionando no porto 5200'. rainbow)
+app.listen (3000, () => {
+    console.log('Server funcionando no porto 3000'. rainbow)
 })
